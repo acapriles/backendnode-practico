433dc00a46fec336dcc8fe8e950c9febfa572ea6
Anderson Capriles <ancapriles@gmail.com>
2020-03-25 18:08:06 -0400

    Last commit

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,5 +1,14 @@
-//const store = require('../../../store/dummy');
-const store = require('../../../store/mysql');
+// const store = require('../../../store/dummy');
+
+const config = require('../../../config');
+
+let store;
+if (config.remoteDB === true) {
+    store = require('../../../store/remote-mysql');
+} else {
+    store = require('../../../store/mysql');
+}
+
 const ctrl = require('./controller');
 
 module.exports = ctrl(store);
--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 module.exports = {
+    remoteDB: process.env.REMOTE_DB || false,
     api: {
         port: process.env.API_PORT || 3000,
+    },
+    post: {
+        port: process.env.POST_PORT || 3002,
     },
     jwt: {
         secret: process.env.JWT_SECRET || 'notasecret!',
@@ -11,4 +15,17 @@ module.exports = {
         password: process.env.MYSQL_PASS || '6Ge3FvupfW',
         database: process.env.MYSQL_DB || '4kifpdCLqd',
     },
+    mysqlService: {
+        host: process.env.MYSQL_SRV_HOST || 'localhost',
+        port: process.env.MYSQL_SRV_PORT || 3001,
+    },
+    cacheService: {
+        host: process.env.MYSQL_SRV_HOST || 'localhost',
+        port: process.env.MYSQL_SRV_PORT || 3003,
+    },
+    redis: {
+        host: process.env.REDIS_HOST || 'redis-13556.c14.us-east-1-3.ec2.cloud.redislabs.com',
+        port: process.env.REDIS_PORT || 13556,
+        password: process.env.REDIS_PASS || '9sWIItAvlPbkoTcwOXstNm9hDFv7AU2F',
+    }
 }
